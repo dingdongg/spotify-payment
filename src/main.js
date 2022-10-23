c07a57158b47d30bdb6e826be0731439576a8a70
Michael <michaeltandyo@gmail.com>
2022-10-23 02:03:37 -0600

    finished the navbar

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
 
+import './assets/main.css'
+import "./assets/reset.css"
 const app = createApp(App)
 
 app.use(router)
