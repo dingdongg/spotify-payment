a3ce0ca80735630bf6fb9bc3f0fb69b7c69399a2
dingdongg <cs.dg.kim@gmail.com>
2022-10-23 01:05:43 -0700

    create stub api handler

---
+++ src/api/api.js
@@ -0,0 +1,7 @@
+const api = {
+    login({ username, password }) {
+        console.log(`API.JS LOGIN STUB\nLOGGING IN WITH USERNAME ${username} AND PASSWORD ${password}`);
+    }
+}
+
+export default api;
