ad274b50c0fd81a457d1fa1c30e5d752ba79f9c0
dingdongg <cs.dg.kim@gmail.com>
2022-10-23 01:05:52 -0700

    create login page view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,5 @@
 <script>
+import api from "../api/api";
 
 export default {
 
@@ -6,29 +7,91 @@ export default {
     return {
       username: "",
       password: "",
-    }
+    };
   },
 
-
   methods: {
-    login() {
-      console.log("LOGGING IN");
+    submitForm() {
+      api.login({ 
+        username: this.username, 
+        password: this.password 
+      });
     }
-  }
+  },
 }
 
 </script>
 
 <template>
-  <form @submit.prevent="login">
-    <div id="username-container">
-      <label for="username">username</label>
-      <input id="username" type="text" v-model="username" />
+  <div class="container">
+    <div class="background-wrapper">
+      <p class="login-form-text">SPOTIFY PAYMENT LOGIN</p>
+      <div class="form-container">
+        <form @submit.prevent="submitForm">
+          <div id="username-container" class="input-container">
+            <input class="clickable" id="username" type="text" v-model="username" placeholder="username" />
+          </div>
+          <div id="password-container" class="input-container">
+            <input class="clickable" type="password" id="password" v-model="password" placeholder="password" />
+          </div>
+          <button type="submit" class="clickable form-submit-button">submit</button>
+        </form>
+      </div>
     </div>
-    <div id="password-container">
-      <label for="password">password</label>
-      <input type="password" id="password" v-model="password" />
-    </div>
-    <button type="submit">submit</button>
-  </form>
+  </div>
 </template>
+
+<style scoped>
+
+.container, .background-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  height: 100vh;
+  background-color: #0b0C10;
+  color: #66FCF1;
+  font-family: roboto, 'Noto Sans Myanmar UI', arial, sans-serif;
+}
+
+.background-wrapper {
+  background-color: #1F2833;
+  border-radius: 5px;
+  padding: 20px;
+  height: 30vh;
+  width: 50vh;
+}
+
+.login-form-text {
+  font-size: 32px;
+  font-weight: bold;
+}
+
+.form-container {
+  padding: 10px;
+  width: 50%;
+}
+
+.form-submit-button {
+  width: 100%;
+}
+
+input, button {
+  font-family: inherit;
+}
+
+input {
+  width: 96%;
+}
+
+.input-container {
+  margin: 10px 0 10px 0;
+}
+
+.clickable {
+  font-size: 20px;
+}
+
+</style>
