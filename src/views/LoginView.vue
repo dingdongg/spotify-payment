f8afe79da9ece92245c657186142be14373a3f8e
dingdongg <cs.dg.kim@gmail.com>
2022-10-22 23:36:34 -0700

    create login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,11 +7,28 @@ export default {
       username: "",
       password: "",
     }
+  },
+
+
+  methods: {
+    login() {
+      console.log("LOGGING IN");
+    }
   }
 }
 
 </script>
 
 <template>
-  <p>{{ username }} {{ password }}</p>
+  <form @submit.prevent="login">
+    <div id="username-container">
+      <label for="username">username</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div id="password-container">
+      <label for="password">password</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">submit</button>
+  </form>
 </template>
